989b31c619149167b01b0e9dd5f638dcf606c971
Mauricio Zanetti Filho <contact@thinkincoin.com>
2023-07-10 23:26:56 -0300

    update

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -87,6 +87,12 @@ const CHAIN_CONFIGS: { [key: number]: ChainConfig } = {
     weth: '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7',
     creationBlock: 31583684,
   },
+  // avalanche
+  [43114]: {
+    router: '0xf0d3EffdcdF9062A3a49F27310e1e8a5629E036B',
+    weth: '0xcF664087a5bB0237a0BAd6742852ec6c8d69A27a',
+    creationBlock: 44134893,
+  },
 }
 
 export const UNIVERSAL_ROUTER_ADDRESS = (chainId: number): string => {
